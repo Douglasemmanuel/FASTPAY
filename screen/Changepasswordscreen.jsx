995345cd8a5f1539216a6b93fbffb910384ae64c
DOUGLAS EMMANUEL <emmanueldouglas2121@gmail.com>
2023-12-12 23:56:47 +0000

    added a changepasswordscreen

---
+++ screen/Changepasswordscreen.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+const Changepasswordscreen = () => {
+  return (
+    <SafeAreaView>
+    <View>
+      <Text>Changepasswordscreen</Text>
+    </View>
+    </SafeAreaView>
+  )
+}
+
+export default Changepasswordscreen
+
+const styles = StyleSheet.create({})
