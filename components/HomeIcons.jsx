a91b6085fe2b09214a1470fc693061b41ad03504
DOUGLAS EMMANUEL <emmanueldouglas2121@gmail.com>
2023-12-18 16:05:49 +0000

    working on home screen

--- components/HomeIcons.jsx
+++ components/HomeIcons.jsx
@@ -2,15 +2,73 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import {faPaperPlane} from "@fortawesome/free-solid-svg-icons/faPaperPlane"
+import {faBuildingColumns} from "@fortawesome/free-solid-svg-icons/faBuildingColumns"
 const HomeIcons = () => {
     const navigation = useNavigation()
   return (
-    <View>
-      <Text>HomeIcons</Text>
+    <View style={{marginLeft:20}}>
+      <View style={{flexDirection:'row'}}>
+      <Text style={{fontWeight:'bold' , fontSize:20 , flex:4}}>Quick Links</Text>
+      <TouchableOpacity style={{ flex:1}} onPress={navigation.navigate('')}>
+      <Text style={{fontWeight:70 , fontSize:20 , color:'grey' }}>See all</Text>
+      </TouchableOpacity>
+      </View>
+      <View style={styles.curvedView}>
+        <TouchableOpacity >
+      <View style={styles.container1} ><FontAwesomeIcon icon={faPaperPlane} size={18}  /></View>
+      <Text  style={styles.text}>Bank Transfer</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+      <View style={styles.container1}><FontAwesomeIcon icon={faPaperPlane} size={18} /></View>
+      <Text style={styles.text} >Airtime</Text>
+      </TouchableOpacity>
+       <TouchableOpacity>
+      <View style={styles.container1}><FontAwesomeIcon icon={faPaperPlane} size={18} /></View>
+      <Text style={styles.text} >Flight</Text>
+      </TouchableOpacity>
+       <TouchableOpacity>
+      <View style={styles.container1}><FontAwesomeIcon icon={faPaperPlane} size={18} /></View>
+      <Text style={styles.text} >Electricity</Text>
+      </TouchableOpacity>
+    </View>
     </View>
   )
 }
 
 export default HomeIcons
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container1: {
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    padding: 15,
+    borderRadius: 50,
+    width: 50,
+    height: 50,
+  
+  },
+  text:{
+    fontSize:12
+  },
+  curvedView: {
+    //   width: 350,
+    //   height: 90,
+    //   backgroundColor: 'white',
+    //   marginLeft:20,
+      marginTop:10,
+      // justifyContent:'center',
+      // alignItems:'center' ,
+      flexDirection:'row' ,  
+      rowGap:30,
+    //   borderRadius: 20, // Adjust this value for the desired curve
+    },
+})
